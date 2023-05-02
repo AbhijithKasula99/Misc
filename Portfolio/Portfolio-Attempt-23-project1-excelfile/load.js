b55f25b370e2f154f87569f52bb5f84f711007ab
AbhijithKasula99 <abhijithkasula99@gmail.com>
2023-05-01 20:07:20 -0400

    Redid loading page

--- Portfolio/Portfolio-Attempt-23-project1-excelfile/load.js
+++ Portfolio/Portfolio-Attempt-23-project1-excelfile/load.js
@@ -1,4 +1,4 @@
 // Wait for the animation to finish and redirect to content
 setTimeout(function() {
   window.location.href = "./content/content.html"; // Replace "content.html" with the URL of your content page
-}, 5000); // Replace "3000" with the duration of your animation in milliseconds
+}, 4500); // Replace "3000" with the duration of your animation in milliseconds

ae158131fad47acc57b88beb7ed0ac407772651c
AbhijithKasula99 <abhijithkasula99@gmail.com>
2023-05-01 21:18:24 -0400

    Lots of changes

--- Portfolio/Portfolio-Attempt-23-project1-excelfile/load.js
+++ Portfolio/Portfolio-Attempt-23-project1-excelfile/load.js
@@ -1,4 +1,19 @@
-// Wait for the animation to finish and redirect to content
+/* // Wait for the animation to finish and redirect to content
 setTimeout(function() {
   window.location.href = "./content/content.html"; // Replace "content.html" with the URL of your content page
 }, 4500); // Replace "3000" with the duration of your animation in milliseconds
+
+ */
+
+
+if (window.innerWidth > 768) { // check if screen size is larger than 768px
+  setTimeout(function() {
+  window.location.href = "./content/content.html"; // Replace "content.html" with the URL of your content page
+  }, 4500); // Replace "4500" with the duration of your animation in milliseconds
+  } else {
+    setTimeout(function() {
+      window.location.href = "./content/content.html"; // Replace "content.html" with the URL of your content page
+      }, 5000);
+  // do something else if screen size is smaller than or equal to 768px
+  // for example, show a message asking the user to view the content on a larger screen
+  }
